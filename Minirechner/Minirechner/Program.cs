fbdd908421161393c485a9b3069cd340d3045179
lukiweiss <02weiluk@rgtfo-me.it>
2020-04-21 14:45:17 +0200

    Addieren

---
+++ Minirechner/Minirechner/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Minirechner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Zahleins = 0;
+            int Zahlzwei = 0;
+            int Operation = 0;
+            int Ergebnis = 0;
+
+
+            Console.WriteLine("Gib die erste Zahl ein:");
+            Zahleins = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Gib die zweite Zahl ein");
+            Zahlzwei = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Wähle: 1 = Addieren, 2 = Subtrahieren, 3 = Multiplizieren, 4 = Dividieren");
+            Operation = Convert.ToInt16(Console.ReadLine());
+
+            if(Operation == 1) //Addieren
+            {
+                Ergebnis = Zahleins + Zahlzwei;
+            }
+            Console.WriteLine("Ergebnis = " + Ergebnis);
+
+
+            Console.ReadKey();
+        }
+    }
+}
